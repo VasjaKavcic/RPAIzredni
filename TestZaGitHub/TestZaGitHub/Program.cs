381c0343524fbde7877a9de14c1f1ed0260ff7eb
Kavcic <kavcic.vasja@gmail.com>
2019-03-06 18:56:16 +0100

    drugi poskus

---
+++ TestZaGitHub/TestZaGitHub/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestZaGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pozdravljen GitHub");
+            Console.ReadLine();
+        }
+    }
+}
